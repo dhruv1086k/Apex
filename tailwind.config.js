c64846335020437d0fe48ce4ff29f8d79ff65275
dhruv <dhruvpal.2702@gmail.com>
2024-06-11 13:39:35 +0530

    landing page commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      colors:{
+        skin: {
+          base1: 'var(--base1)'
+        }
+      },
+      fontSize: {
+
+      },
+      height: {
+        '15v' : '15vh',
+        '85v' : '85vh',
+        '200' : '200px'
+      },
+      width: {
+        '5p' : '5%',
+        '30p' : '30%',
+        '70p' : '70%'
+      },
+      letterSpacing: {
+        'extra' : '0.5em',
+        'superextra' : '0.8em',
+      },
+      borderRadius: {
+        'circle' : '50%'
+      }
+    },
+  },
+  plugins: [],
+}
+
